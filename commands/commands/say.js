0cf499bb41dc7713eae4333f7dcda81f6f2b20ca
NepAttorney <61463355+NepAttorney@users.noreply.github.com>
2021-05-09 17:49:05 +0300

    Update say.js

--- commands/commands/say.js
+++ commands/commands/say.js
@@ -4,14 +4,6 @@ module.exports = {
     description: "Make me say something.",
     aliases: [],
     execute(client, message, args, discord){
-
-        //This will check if the message contains any bad words.
-        //const meanyWords = ["pussy", "Pussy", "dick", "Dick", "penis", "Penis", "piss", "Piss", "sex", "Sex", "vagina", "Vagina", "nipple", "Nipple", "nipples", "Nipples", "cunt", "Cunt", "asshole", "Asshole", "piss", "Piss"]
-
-        //for (var i=0; i < meanyWords.length; i++) {
-        //    if (message.content.includes(meanyWords[i])) return message.channel.send("Sorry, but I can't say any bad words.");
-        //}
-        
         let messageArgs = args.join(' ');
 
         if(!messageArgs) return;
